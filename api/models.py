5b8dfb41b8e2d3bbdd7f85093d247f3b283bc920
Adal Cima <cima.adalberto@gmail.com>
2020-08-04 21:14:10 -0500

    Add models and run migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    value = models.FloatField()
+    discount_value = models.FloatField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
